f81b4a98aa3c5063df621f1892eb81a2267c42a8
Raúl Cruz <rcruzper@gmail.com>
2020-09-01 19:55:29 +0200

    apply standardjs style guide

--- test/functional/metrics.test.js
+++ test/functional/metrics.test.js
@@ -1,35 +1,31 @@
-'use strict';
+'use strict'
 
-const request = require('supertest');
-const express = require('express');
-const expect = require('chai').expect;
-const actuator = require('../../lib');
+const request = require('supertest')
+const express = require('express')
+const expect = require('chai').expect
+const actuator = require('../../lib')
 
-let app;
+let app
 
-describe('GET /metrics', function() {
-    beforeEach(function() {
-        app = express();
-        app.use(actuator());
-    });
+describe('GET /metrics', function () {
+  beforeEach(function () {
+    app = express()
+    app.use(actuator())
+  })
 
-    afterEach(function() {
-        app.close;
-    });
-
-    it('should return metrics', function(done) {
-        request(app)
-            .get('/metrics')
-            .end(function(err, res) {
-                expect(res.statusCode).to.equal(200);
-                expect(res.headers['content-type']).to.equal('application/json; charset=utf-8');
-                expect(res.body.mem.external).to.be.a("number");
-                expect(res.body.mem.heapTotal).to.be.a("number");
-                expect(res.body.mem.heapUsed).to.be.a("number");
-                expect(res.body.mem.rss).to.be.a("number");
-                expect(res.body.uptime).to.be.a("number");
-                done();
-            });
-    });
-
-});
+  it('should return metrics', function (done) {
+    request(app)
+      .get('/metrics')
+      .end(function (err, res) {
+        if (err) return err
+        expect(res.statusCode).to.equal(200)
+        expect(res.headers['content-type']).to.equal('application/json; charset=utf-8')
+        expect(res.body.mem.external).to.be.a('number')
+        expect(res.body.mem.heapTotal).to.be.a('number')
+        expect(res.body.mem.heapUsed).to.be.a('number')
+        expect(res.body.mem.rss).to.be.a('number')
+        expect(res.body.uptime).to.be.a('number')
+        done()
+      })
+  })
+})
